f29d70bd751298438d63409a031c0ec2abb77310
KianSalehi <kian.k.salehi@gmail.com>
2022-05-16 17:52:59 -0400

    - The intial page when the app is loaded has been changed to eb the index route of the product controller to the list of products - DRY cleaning has been done on the code to remove redundant code form the files - Flash messages for the CRUD functionalities has been moved to its own partial file - The form section of edit.html.erb and new.html.erb has been moved to its own partial file - Weight entries has been changed to include the lbs unit - CSV export added to the show page - Weather API end point added to retrieve the weather info in a private function in the products controller file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,42 @@
-class ProductsController < ApplicationController
+require 'net/http'
+require 'json'
 
+class ProductsController < ApplicationController
+    # Before action used to set the product needed for show, edit, update, and destroy function
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+    # Index function, aka the dashboard which uses weather_description private function 
+    # to get the weather text information and show the view index.html.erb
     def index
         @products = Product.all
+        @products.each do |product|
+            set_weather_description(product)
+            product.weather_description = @weather_description.capitalize()
+            product.save
+        end
     end
 
+    # New function to create a new product in the inventory and show the view new.html.erb
     def new
         @product = Product.new
     end
 
+    # Show function to show a specific product from the database and show the view show.html.erb
     def show
-        @product = Product.find(params[:id])
+        respond_to do |format|
+            format.html
+            format.csv { send_data @product.to_csv}
+        end
     end
 
+    # Edit function to edit a specific product from the database and show the view edit.html.erb
     def edit
-        @product = Product.find(params[:id])
+
     end
 
+    # Update function to submit a specific product to the database with the new values entered by the user
     def update
-        @product = Product.find(params[:id])
-        if @product.update(params.require(:product).permit(:title, :description, :quantity, :weight, :city))
+        if @product.update(product_params)
             flash[:notice] = "Product was updated successfully."
             redirect_to @product
         else
@@ -26,8 +44,9 @@ class ProductsController < ApplicationController
         end
     end
 
+    # Create function to create a new product and submit to the database using the values entered by the user
     def create
-        @product = Product.new(params.require(:product).permit(:title, :description, :quantity, :weight, :city))
+        @product = Product.new(product_params)
         
         if @product.save
             flash[:notice] = "Product was created successfully."
@@ -37,9 +56,50 @@ class ProductsController < ApplicationController
         end
     end
 
+    # Destroy function to delete a product in the database
     def destroy
-        @product = Product.find(params[:id])
         @product.destroy
         redirect_to products_path
+    end
+
+    # Private functions
+    private
+    
+    # Function used to get and set a specific product for operations
+    def set_product
+        @product = Product.find(params[:id])
+    end
+
+    # Function used to get the values entered by the user through the route
+    def product_params
+        params.require(:product).permit(:title, :description, :quantity, :weight, :city)
+    end
+    
+    # Function to retrieve the weather description from OpenWeather API
+    def set_weather_description (product)
+        lon = '0'
+        lat = '0'
+
+        if product.city == "Montreal"
+            lat = '45.5017'
+            lon = '73.5673'
+        elsif product.city == "Ottawa"
+            lat = '45.5017'
+            lon = '75.6972'
+        elsif product.city == "Toronto"
+            lat = '43.6532'
+            lon = '79.3832'
+            p lat
+        elsif product.city == "Vancouver"
+            lat = '49.2827'
+            lon = '123.1207'
+        end
+
+        uri = URI('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=535537cfa52d51c18a868e18b2fdd789')
+        response = Net::HTTP.get_response(uri)
+        weather_response = response.body
+        json_response = JSON.parse(weather_response)
+        @weather_description = json_response["weather"][0]["description"]
+
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,15 @@ class Product < ApplicationRecord
     validates :title, presence: true, length: {minimum: 5, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 500}
     validates :city, presence: true
-    validates :quantity, presence: true
+    validates :quantity, presence: true, numericality: {greater_than: 0}
+    validates :weight, numericality: {greater_than: 0}, allow_blank: true
+    
+    # Model function to generate CSV file for a specific product
+    def to_csv
+        headers = ["id", "title", "description", "quantity", "weight", "city", "weather_description"]
+        CSV.generate(headers: true) do |csv|
+            csv << headers
+            csv << self.attributes.values_at(*headers)
+        end
+    end
 end
