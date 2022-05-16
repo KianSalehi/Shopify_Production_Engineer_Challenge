3dfb74270d0962359c6d5f8d75601e21663f4cac
KianSalehi <kian.k.salehi@gmail.com>
2022-05-15 22:37:42 -0400

    -CRUD functionality added to the app. -edit html modified to show the validation comments -show html modified to include edit and delete button -new html modified to show the validation comments -application html modified to include flash messages -index html modified to include the action buttons -products controller file edited to include CRUD functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,42 @@ class ProductsController < ApplicationController
         @products = Product.all
     end
 
+    def new
+        @product = Product.new
+    end
+
+    def show
+        @product = Product.find(params[:id])
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(params.require(:product).permit(:title, :description, :quantity, :weight, :city))
+            flash[:notice] = "Product was updated successfully."
+            redirect_to @product
+        else
+            render 'edit'
+        end
+    end
+
+    def create
+        @product = Product.new(params.require(:product).permit(:title, :description, :quantity, :weight, :city))
+        
+        if @product.save
+            flash[:notice] = "Product was created successfully."
+            redirect_to products_path(@product)
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        redirect_to products_path
+    end
 end
